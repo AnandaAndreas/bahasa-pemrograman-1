56070c48c2c338b545c0ef291ca2ae4de3bd79e7
AnandaAndreas <anandaandreas@students.amikom.ac.id>
2023-03-23 21:49:01 +0700

    first commit

---
+++ src/main/kotlin/pertemuan1.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("hello World")
+    println("Ananda")
+
+    print("Pekerjaan : ")
+    var pekerjaan = readln()
+
+    print("Alamat : ")
+    var alamat = readln()
+
+    println("pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+}
