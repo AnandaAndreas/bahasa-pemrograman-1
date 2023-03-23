035ddc39f328b1f1e01ceb6b17cb2ab95ed98f12
AnandaAndreas <anandaandreas@students.amikom.ac.id>
2023-03-23 22:02:11 +0700

    pertemuan2

--- src/main/kotlin/pertemuan1.kt
+++ src/main/kotlin/pertemuan1.kt
@@ -1,12 +1,20 @@
 fun main() {
-    println("hello World")
-    println("Ananda")
-
-    print("Pekerjaan : ")
-    var pekerjaan = readln()
-
-    print("Alamat : ")
-    var alamat = readln()
-
-    println("pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+//    println("hello World")
+//    println("Ananda")
+//
+//    print("Pekerjaan : ")
+//    var pekerjaan = readln()
+//
+//    print("Alamat : ")
+//    var alamat = readln()
+//
+//    println("pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+    //pertemuan 2
+    var namaLengkap : String = "Ananda Andreas"
+    var alamat : String
+    alamat = "Yogyakarta"
+    var umur = 22
+    println(namaLengkap)
+    println(alamat)
+    println(umur)
 }
